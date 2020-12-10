5de92039c4194a52ae9ee79d086d7c6789d6fdeb
AWSiegfried <awsiegfried@gmail.com>
2020-12-10 08:25:58 -0800

    created database files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Burnin Love Burger');
+INSERT INTO burgers (burger_name) VALUES ('Fiesta Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Siegfried', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Smash Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Blue Cheese Sliders');
